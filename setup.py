1916ca6ece366a6305cdafb6ba2a923bf693ddbc
Rasmus Munk <munk1@live.dk>
2021-02-03 11:47:56 +0100

    General package structure update, deprecated Python 2.7/3.5 while enabling support for Python 3.8/3.9

--- setup.py
+++ setup.py
@@ -1,29 +1,50 @@
 import sys
 from setuptools import find_packages
 
-# Python 2 install
-if sys.version_info[0] >= 3:
-    from distutils.core import setup
-else:
-    from setuptools import setup
+here = os.path.dirname(__file__)
 
-with open("README.rst") as r_file:
-    long_description = r_file.read()
+def read(path):
+    with open(path, "r") as _file:
+        return _file.read()
+
+
+def read_req(name):
+    path = os.path.join(here, name)
+    return [req.strip() for req in read(path).splitlines() if req.strip()]
+
+
+version_ns = {}
+with open(os.path.join(here, "version.py")) as f:
+    exec(f.read(), {}, version_ns)
+
+long_description = open("README.rst").read()
 
 setup(
     name="mig-utils",
-    version="0.1.7.4",
-    long_description=long_description,
+    version=version_ns["__version__"],
     description="Minimum Intrusion Grid (MiG) Utilities library",
-    url="https://github.com/rasmunk/mig_utils",
+    long_description=long_description,
     author="Rasmus Munk",
     author_email="munk1@live.dk",
+    license="MIT",
+    keywords="Staging Data Job Input/Output",
+    url="https://github.com/rasmunk/mig_utils",
     packages=find_packages(),
-    install_requires=[
-        "fs.sshfs==0.8.0",
-        "fs==2.0.27",
-        "ssh2-python==0.26.0",
-        "six==1.15",
+    install_requires=read_req("requirements.txt"),
+    extras_require={
+        "test": read_req("tests/requirements.txt"),
+        "dev": read_req("requirements-dev.txt"),
+    },
+    project_urls={"Source Code": "https://github.com/rasmunk/mig_utils"},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    include_package_data=True,
 )
