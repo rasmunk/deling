9234a93dad7882d812a36077a82b1b1733db6a57
Rasmus Munk <munk1@live.dk>
2024-01-26 19:04:19 +0100

    Move the generated ssh keys into a test directory

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,7 @@ import os
 from random import random
 from deling.authenticators.ssh import SSHAuthenticator
 from deling.io.datastores.core import SFTPStore
-from deling.utils.io import exists, makedirs
+from deling.utils.io import exists, makedirs, removedirs
 from deling.authenticators.ssh import (
     gen_ssh_key_pair,
     load_rsa_key_pair,
@@ -28,13 +28,20 @@ class AuthenticationTestCases:
     def test_ed25519_file_key_authentication(self):
         key_type = "ed25519"
         key_name = "id_{}_{}".format(key_type, self.seed)
-        self.assertTrue(gen_ssh_key_pair(key_name=key_name, key_type=key_type))
+        self.assertTrue(
+            gen_ssh_key_pair(
+                default_ssh_dir=self.test_ssh_dir, key_name=key_name, key_type=key_type
+            )
+        )
         self.assertTrue(
             ssh_credentials_exists(
+                default_ssh_dir=self.test_ssh_dir,
                 key_name=key_name,
             )
         )
-        ssh_credentials = load_rsa_key_pair(key_name=key_name)
+        ssh_credentials = load_rsa_key_pair(
+            default_ssh_dir=self.test_ssh_dir, key_name=key_name
+        )
         self.assertIsNotNone(ssh_credentials)
         datastore = SFTPStore(
             host="127.0.0.1",
@@ -47,16 +54,21 @@ class AuthenticationTestCases:
         self.assertTrue(datastore.is_connected())
         datastore.disconnect()
         self.assertFalse(datastore.is_connected())
-        self.assertTrue(remove_ssh_credentials(key_name=key_name))
-        self.assertFalse(ssh_credentials_exists(key_name=key_name))
+        self.assertTrue(
+            remove_ssh_credentials(default_ssh_dir=self.test_ssh_dir, key_name=key_name)
+        )
+        self.assertFalse(
+            ssh_credentials_exists(default_ssh_dir=self.test_ssh_dir, key_name=key_name)
+        )
 
 
 class SFTPStoreTestED25519KeyAuthentication(AuthenticationTestCases, unittest.TestCase):
     def setUp(self):
         self.seed = str(random())[2:10]
         tmp_test_dir = os.path.join(os.getcwd(), "tests", "tmp")
-        if not exists(tmp_test_dir):
-            self.assertTrue(makedirs(tmp_test_dir))
+        self.test_ssh_dir = os.path.join(tmp_test_dir, "ssh-{}".format(self.seed))
+        if not exists(self.test_ssh_dir):
+            self.assertTrue(makedirs(self.test_ssh_dir))
 
         self.username = "mountuser"
         self.password = "Passw0rd!"
@@ -68,4 +80,9 @@ class SFTPStoreTestED25519KeyAuthentication(AuthenticationTestCases, unittest.Te
         # For now, use ED25519 keys
 
     def tearDown(self):
+        # TODO, remove everything inside the self.test_ssh_dir before it
+        # is removed
+        # Remove every file from test_ssh_dir
+        if exists(self.test_ssh_dir):
+            self.assertTrue(removedirs(self.test_ssh_dir, recursive=True))
         self.share = None
