5694728be86d5d8e8b1a11a538ea27b81c0350ce
Rasmus Munk <rasmunk@users.noreply.github.com>
2026-02-11 15:14:37 +0100

    Remove the deprecated license classifier

--- setup.py
+++ setup.py
@@ -58,7 +58,6 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
